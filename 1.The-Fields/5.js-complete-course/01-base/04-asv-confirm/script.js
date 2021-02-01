f18064f1ea2749c45c7cfcb58aca4acd74a48625
Julio Cesar <juliocesarteixeira1992@gmail.com>
2021-02-01 15:50:15 +0100

    revised

--- 1.The-Fields/5.js-complete-course/01-base/04-asv-confirm/script.js
+++ 1.The-Fields/5.js-complete-course/01-base/04-asv-confirm/script.js
@@ -3,8 +3,16 @@ function myInfo() {
     let gender = prompt('Which gender do you identify yourself with?');
     let city = prompt('Which city do you live in?');
     let check = confirm("You are " + age + ", you identify yourself as " + gender + " and you live in " + city);
-    if (check !== true){
+    if (!check){
         return(myInfo());
     }
 }
 myInfo();
+
+
+//do{
+// prompts
+//confirm(age+"\n"+...)
+// }
+// while (!positive)
+
